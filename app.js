870b94bc6c5767c3951df470713ecb0591fe2340
Jacob Hileman <jacobh@Jacobs-iMac-2.local>
2015-06-18 23:14:59 -0400

    new text

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello wrong');
+  res.send('hello baby! it works!');
 });
  
 app.listen(process.env.PORT || 5000);
